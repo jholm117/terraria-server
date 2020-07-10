255ab84ad77e229adeecd80b7ff788605116f7f3
jholm117 <jholm117@gmail.com>
2020-07-10 16:18:36 -0700

    update load and save

--- load.sh
+++ load.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-git pull --ff-only
+
 terraria_dir="$(dirname "$(dirname "$(realpath $0)")")"
-world_file=`find "$terraria_dir/terraria-server" -type f -name "*.wld"` 
+repo_dir="$terraria_dir/terraria-server"
+cd "$repo_dir"
+git pull --ff-only
+world_file=`find "$repo_dir" -type f -name "*.wld"` 
 
 cp "$world_file" "$terraria_dir/Worlds"
--- save.sh
+++ save.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 terraria_dir="$(dirname "$(dirname "$(realpath $0)")")"
-world_file_path=`find "$terraria_dir/terraria-server" -type f -name "*.wld"`
+repo_dir="$terraria_dir/terraria-server"
+cd "$repo_dir"
+
+world_file_path=`find "$repo_dir" -type f -name "*.wld"`
 world_filename=`basename "$world_file_path"`
-cp "$terraria_dir/Worlds/$world_filename" "$terraria_dir/terraria-server"
+cp "$terraria_dir/Worlds/$world_filename" "$repo_dir"
 git add -A
 git commit -m 'Update world file'
 git push origin master

4950e7d062cee7ea19782b01b45eb4a4690bef73
jholm117 <jholm117@gmail.com>
2020-07-10 15:42:47 -0700

    change terrariaserver

--- load.sh
+++ load.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull --ff-only
 terraria_dir="$(dirname "$(dirname "$(realpath $0)")")"
-world_file=`find "$terraria_dir/shared-server" -type f -name "*.wld"` 
+world_file=`find "$terraria_dir/terraria-server" -type f -name "*.wld"` 
 
 cp "$world_file" "$terraria_dir/Worlds"
